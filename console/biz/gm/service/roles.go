e316ac87bf027dfe770f8fe5aaf977f4e63c3b8e
zhangjie <zhangjie@tcxa.com.cn>
2023-03-14 16:57:15 +0800

    add: build code

--- console/biz/gm/service/roles.go
+++ console/biz/gm/service/roles.go
@@ -7,15 +7,37 @@ import (
 	"gorm.io/gorm"
 )
 
-func GetRoles(uid int) ([]model.CharacInfo, error) {
+func GetRoles(uid int) ([]RoleResult, error) {
 	db1 := game_db.DBPools.Get(model.DTaiwan)
 	err := db1.First(&model.Accounts{}, uid).Error
 
-	var data = make([]model.CharacInfo, 0)
 	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return data, errors.New("该账号不存在")
+		return nil, errors.New("该账号不存在")
 	}
+
+	var data = make([]model.CharacInfo, 0)
 	dbx := game_db.DBPools.Get(model.TaiwanCain)
 	dbx.Table("charac_info").Where("m_id = ?", uid).Find(&data)
-	return data, nil
+
+	var result = make([]RoleResult, 0)
+	for _, info := range data {
+		result = append(result, RoleResult{
+			CharacInfo: info,
+			Money:      getMoneyByRole(info.CharacNo),
+		})
+	}
+
+	return result, nil
+}
+
+func getMoneyByRole(characNo int) int {
+	dbx := game_db.DBPools.Get(model.TaiwanCain2nd)
+
+	type _R struct {
+		Money int
+	}
+
+	var data _R
+	dbx.Table("inventory").Where("charac_no = ?", characNo).Take(&data)
+	return data.Money
 }
--- web/README.md
+++ web/README.md
@@ -1,1 +1,3 @@
 # WEB
+
+https://bbs.aladedalu.com/thread-3143-1-1.html
