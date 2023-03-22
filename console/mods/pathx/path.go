8f63da347cc48bc48874f868decdb4d8315b4d13
zhangjie <zhangjie@tcxa.com.cn>
2023-03-22 15:30:21 +0800

    add fmt.print

--- console/mods/pathx/path.go
+++ console/mods/pathx/path.go
@@ -12,6 +12,10 @@ func getWorkDir() (string, error) {
 }
 
 // GetExeDir DEBUG=1, 将会跑 工作目录
+/*
+1. 目的：在exe（程序下）自动生成 一些目录和默认配置文件
+2. GetWd 跟 Executable ，但是 GetWd 类型于 pwd 工作目录，在go run 跑必会是 GetWd = Executable
+*/
 func GetExeDir() (string, error) {
 	// 程序目录， exe 二进制文件路径
 	ex, err := os.Executable()
